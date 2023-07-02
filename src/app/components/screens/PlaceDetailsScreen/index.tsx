8f71a54742daadb8385b18cd3743f8f617700c05
montexbjeliseo <secondmtx@gmail.com>
2023-07-02 16:50:06 -0300

    Locations details

--- src/app/components/screens/PlaceDetailsScreen/index.tsx
+++ src/app/components/screens/PlaceDetailsScreen/index.tsx
@@ -1,9 +1,54 @@
-function PlaceDetailsScreen(){
+import { Link, useParams } from "react-router-dom";
+
+import styles from "./styles.module.css";
+import { useGetData } from "../../../hooks/useGetDataR";
+function PlaceDetailsScreen() {
+  const { id } = useParams();
+
+  const { data, error, loading } = useGetData(
+    `https://rickandmortyapi.com/api/location/${id}`
+  );
+
+  if (loading) {
     return (
-        <div>
-            <h1>PlaceDetails Screen works!</h1>
-        </div>
+      <main>
+        <h1>Cargando...</h1>
+      </main>
     );
+  }
+
+  if (error) {
+    return (
+      <main>
+        <h1>Error en la petición!</h1>
+        <p>{error}</p>
+      </main>
+    );
+  }
+
+  console.log(data, error, loading);
+
+  return (
+    <main>
+      {data && (
+        <>
+          <Link className={styles.backLink} to="/places">
+            Volver atrás
+          </Link>
+          <h1>{data.name}</h1>
+          <p>
+            <strong>Dimension:</strong> {data.dimension}
+          </p>
+          <p>
+            <b>Type:</b> {data.type}
+          </p>
+          <p>
+            <b>Type:</b> {data.type}
+          </p>
+        </>
+      )}
+    </main>
+  );
 }
 
 export default PlaceDetailsScreen;
